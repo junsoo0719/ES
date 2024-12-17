c2c3a4e4a624e213df2fffecea8c48d70ae4c89d
junsoo0719 <junsoo0719@gmail.com>
2024-12-18 02:09:27 +0900

    최종프로젝트

--- lab1.c
+++ lab1.c
@@ -9,22 +9,24 @@
 #define ATS75_CONFIG_REG 1
 #define ATS75_TEMP_REG 0
 #define ATS75_ADDR 0x98 // 0b10011000, 7비트를 1비트 left shift
+#define CDS_VALUE 871
+
 
 #define  TASK_STK_SIZE  OS_TASK_DEF_STK_SIZE
-#define  N_TASKS        4
-
+#define  N_TASKS        5
 
 
 OS_STK		TaskStk[N_TASKS][TASK_STK_SIZE];
 OS_EVENT* Mbox;
-
-// Event flag 선언하세요.
 OS_FLAG_GRP* EventFlag;
+
 UCHAR send[4];
+USHORT light_value;
 
 void write_twi_1byte_nopreset(UCHAR reg, UCHAR data);
 void write_twi_0byte_nopreset(UCHAR reg);
 void  TemperatureTask(void* data);
+void  ADCTask(void* data);
 void  FndTask(void* data);
 void  FndDisplayTask(void* data);
 void  LEDTask(void* data);
@@ -40,15 +42,15 @@ int main(void)
 
 	DDRA = 0xff;
 
-	// // Event flag 생성하세요. 
 	INT8U	err;
 	EventFlag = OSFlagCreate(0x00, &err);
 	Mbox = OSMboxCreate((void*)0);
 
 	OSTaskCreate(TemperatureTask, (void*)0, (void*)&TaskStk[0][TASK_STK_SIZE - 1], 0);
-	OSTaskCreate(FndTask, (void*)0, (void*)&TaskStk[1][TASK_STK_SIZE - 1], 1);
-	OSTaskCreate(LEDTask, (void*)0, (void*)&TaskStk[2][TASK_STK_SIZE - 1], 2);
-	OSTaskCreate(FndDisplayTask, (void*)0, (void*)&TaskStk[3][TASK_STK_SIZE - 1], 3);
+	OSTaskCreate(ADCTask, (void*)0, (void*)&TaskStk[1][TASK_STK_SIZE - 1], 1);
+	OSTaskCreate(FndTask, (void*)0, (void*)&TaskStk[2][TASK_STK_SIZE - 1], 2);
+	OSTaskCreate(LEDTask, (void*)0, (void*)&TaskStk[3][TASK_STK_SIZE - 1], 3);
+	OSTaskCreate(FndDisplayTask, (void*)0, (void*)&TaskStk[4][TASK_STK_SIZE - 1], 4);
 	OSStart();
 
 	return 0;
@@ -93,17 +95,55 @@ int ReadTemperature(void)
 	return value;
 }
 
+void InitADC() 
+{
+	ADMUX = 0x00;
+	ADCSRA = 0x87;
+}
+
+USHORT ReadADC()
+{
+	unsigned char adc_low, adc_high;
+	unsigned short value;
+	ADCSRA |= 0x40; // ADC start conversion, ADSC = '1'
+	while ((ADCSRA & (0x10)) != 0x10); // ADC 변환 완료 검사
+	adc_low = ADCL;
+	adc_high = ADCH;
+	value = (adc_high << 8) | adc_low;
+
+	return value;
+}
+
 void TemperatureTask(void* data)
 {
 	data = data;
 	USHORT value;
 	InitI2C();
 
-	write_twi_1byte_nopreset(ATS75_CONFIG_REG, 0x00); // 9비트, Normal
+	write_twi_1byte_nopreset(ATS75_CONFIG_REG, 0x00);
 	write_twi_0byte_nopreset(ATS75_TEMP_REG);
 	while (1) {
 		value = ReadTemperature();
 		OSMboxPost(Mbox, (void*)&value);
+		OSTimeDlyHMSM(0, 0, 1, 0);
+	}
+}
+
+void ADCTask(void* data)
+{
+	INT8U	err;
+	InitADC();
+
+	while (1) {
+		light_value = ReadADC();
+		if (light_value < CDS_VALUE) {
+			OSFlagPost(EventFlag, 0x02, OS_FLAG_SET, &err);
+			OSFlagPost(EventFlag, 0x04, OS_FLAG_CLR, &err);
+		}
+		else {
+			OSFlagPost(EventFlag, 0x04, OS_FLAG_SET, &err);
+			OSFlagPost(EventFlag, 0x02, OS_FLAG_CLR, &err);
+		}
 		OSTimeDlyHMSM(0, 0, 1, 0);
 	}
 }
@@ -119,12 +159,12 @@ void FndTask(void* data)
 	while (1) {
 		now_value = *(USHORT*)OSMboxPend(Mbox, 0, &err);
 
-		if ((now_value & 0x8000) != 0x8000)  // Sign 비트 체크
+		if ((now_value & 0x8000) != 0x8000)
 			num[3] = 11;
 		else
 		{
 			num[3] = 10;
-			now_value = (~now_value) - 1;   // 2’s Compliment
+			now_value = (~now_value) - 1;
 		}
 
 		value_int = (UCHAR)((now_value & 0x7f00) >> 8);
@@ -134,8 +174,6 @@ void FndTask(void* data)
 		num[1] = value_int % 10;
 		num[0] = ((value_deci & 0x80) == 0x80) * 5;
 
-
-		// send array 채우고 OSFlagPost 함수 호출하세요. 
 		for (i = 0; i < 4; i++)
 			send[i] = num[i];
 		OSFlagPost(EventFlag, 0x01, OS_FLAG_SET, &err);
@@ -147,24 +185,13 @@ void FndTask(void* data)
 void LEDTask(void* data)
 {
 	INT8U	err;
-	UCHAR	value_int;
-	UCHAR   value_result = 0;
-	UCHAR* msg;
-	int i;
 
 	while (1) {
-		// OSFlagPend 함수 호출하고 출력하세요. 
-		OSFlagPend(EventFlag, 0x01, OS_FLAG_WAIT_SET_ANY, 0, &err);
+		OSFlagPend(EventFlag, 0x02, OS_FLAG_WAIT_SET_ANY, 0, &err);
+		PORTA = 0xff;
 
-		for (i = 0; i < 3; i++) {
-			value_int = send[i];
-			if (value_int > 7)
-				value_result |= (1 << 7);
-			else
-				value_result |= (1 << value_int);
-		}
-		PORTA = value_result;
-		value_result = 0;
+		OSFlagPend(EventFlag, 0x04, OS_FLAG_WAIT_SET_ANY, 0, &err);
+		PORTA = 0x00;
 
 		OSTimeDlyHMSM(0, 0, 1, 0);
 	}
@@ -175,21 +202,17 @@ void FndDisplayTask(void* data)
 {
 	UCHAR FND_DATA[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7c, 0x07, 0x7f, 0x67, 0x40, 0x00 };
 	UCHAR fnd_sel[4] = { 0x01, 0x02, 0x04, 0x08 };
-	UCHAR* display;
 	INT8U	err;
 	int i;
 
 	DDRC = 0xff;
 	DDRG = 0x0f;
 
-	// OSFlagPend 함수 호출하세요 
-	OSFlagPend(EventFlag, 0x01, OS_FLAG_WAIT_SET_ANY, 0, &err);
-
 	while (1)
 	{
+		OSFlagPend(EventFlag, 0x01, OS_FLAG_WAIT_SET_ANY, 0, &err);
 		for (i = 0; i < 4; i++)
 		{
-			// 출력하세요.
 			PORTC = FND_DATA[send[i]];
 			PORTG = fnd_sel[i];
 			if (i == 1) PORTC |= 0x80;
